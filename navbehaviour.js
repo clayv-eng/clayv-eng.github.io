22a6802c31e428632fb0a3eae95e9b085b11a65a
ClaytonJarrett <119470233+clayv-eng@users.noreply.github.com>
2023-03-13 17:12:50 -0400

    Add files via upload
    Added script for nav behaviour

---
+++ navbehaviour.js
@@ -0,0 +1,13 @@
+var linkContainer = document.getElementById("navbarWrapper");
+
+// Get all buttons with class="btn" inside the container
+var links = linkContainer.getElementsByClassName("nav-link");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+    });
+}
